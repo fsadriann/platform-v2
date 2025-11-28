640b6b4e5cdf8618797cdec5a336c1c55d128aa4
fsadriann <adrianfelipeforero@gmail.com>
2025-11-28 17:47:27 -0500

    se añadieron los efectos de sonido y los fixes a los bugs de colisión

--- 5 - fixes/code/main.py
+++ 5 - fixes/code/main.py
@@ -6,15 +6,23 @@ from ui import UI
 
 class Game:
     def __init__(self):
+
         # game attributes
-        self.max_level = 0
+        self.max_level = 2
         self.max_health = 100
         self.current_health = 100
         self.coins = 0
 
+        # audio
+        self.level_bg_music = pygame.mixer.Sound('audio/level_music.wav')
+        self.level_bg_music.set_volume(0.1)
+        self.overworld_bg_music = pygame.mixer.Sound('audio/overworld_music.wav')
+        self.overworld_bg_music.set_volume(0.1)
+
         # overworld creation
         self.overworld = Overworld(0, self.max_level, screen, self.create_level)
         self.status = 'overworld'
+        self.overworld_bg_music.play(loops = -1)
 
         # user interface
         self.ui = UI(screen)
@@ -22,12 +30,16 @@ class Game:
     def create_level(self, current_level):
         self.level = Level(current_level, screen, self.create_overworld, self.change_coins, self.change_health)
         self.status = 'level'
+        self.overworld_bg_music.stop()
+        self.level_bg_music.play(loops = -1)
 
     def create_overworld( self, current_level, new_max_level):
         if new_max_level > self.max_level:
             self.max_level = new_max_level
         self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
         self.status = 'overworld'
+        self.overworld_bg_music.play(loops = -1)
+        self.level_bg_music.stop()
 
     def change_coins(self, amount):
         self.coins += amount
@@ -42,6 +54,8 @@ class Game:
             self.max_level = 0
             self.overworld = Overworld(0, self.max_level, screen, self.create_level)
             self.status = 'overworld'
+            self.overworld_bg_music.play(loops = -1)
+            self.level_bg_music.stop()
 
     def run(self):
         if self.status == 'overworld':
